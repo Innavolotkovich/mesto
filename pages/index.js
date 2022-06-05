5fd51e2ee7b54c47303eeeb891b5a5187bc4bb1f
unknown <innavolotkovich@yahoo.com>
2022-06-05 13:43:03 +0300

    js+popup+headerlogo

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,57 @@
-const button = document.querySelector('.profile__edit-button');
+const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 
-function closePopup() {
-
+function closePopupOnQ(e) {
+    if (e.code === 'KeyQ'){
+    closePopup();
+    }
 }
 
-button.addEventListener('click', function () {
-    popup.classList.remove('popup_hidden')
+function openPopup() {
+    popup.classList.remove('popup_hidden');
+    document.addEventListener('keypress', closePopupOnQ);
+}
+
+function closePopup() {
+    popup.classList.add('popup_hidden');
+    document.removeEventListener('keypress', closePopupOnQ);
+}
+
+popupOpenButton.addEventListener('click', function () {
+    openPopup();
 
 })
 
 popupCloseButton.addEventListener('click', function () {
-    popup.classList.add('popup_hidden');
+    closePopup();
 
 });
 
-popup.addEventListener('click', function (e)  {
+popup.addEventListener('click', function (e) {
     if (e.target === e.currentTarget) {
-    popup.classList.add('popup_hidden');
+    closePopup();
     }
     });
 
-    document.addEventListener('keypress', function(e) {
-        if (e.)
+
+    
+    let formElement = document.querySelector('.popup__form');
+    let nameInput = document.querySelector('.profile__title'); 
+    let jobInput =  document.querySelector('.profile__subtitle');  
+
+    function formSubmitHandler (evt) {
+        evt.preventDefault();
+
+        let newName = nameInput.value;
+        let newJob = jobInput.value;
+
+        // let nameForm = formElement.querySelector('.popup__form_name');
+        // let jobForm = formElement.querySelector('.popup__form_job');
+
+        formElement.textContent = `newName`;
+        formElement.textContent = `newJob`;
+
     }
 
+    formElement.addEventListener('submit', formSubmitHandler); 
