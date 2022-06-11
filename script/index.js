348fefbd9a9307b260f9a3fc0a6ddf872fbabf5b
unknown <innavolotkovich@yahoo.com>
2022-06-11 15:57:37 +0300

    correction revier

--- script/index.js
+++ script/index.js
@@ -2,61 +2,83 @@ const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__form_name'); 
-const jobInput =  formElement.querySelector('.popup__form_job'); 
+const formPopup = document.querySelector('.popup__form');
+const nameInput = formPopup.querySelector('.popup__item_type_name'); 
+const jobInput =  formPopup.querySelector('.popup__item_type_job'); 
 
 const profileName = document.querySelector('.profile__title'); 
 const profileJob =  document.querySelector('.profile__subtitle'); 
 
 
+popupOpenButton.addEventListener('click', function () {
+    popup.classList.remove('popup_hidden');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }); 
+
+
+popupCloseButton.addEventListener('click', function () {
+    popup.classList.add('popup_hidden');
+    
+  }); 
+
+
+formPopup.addEventListener('submit', function (e) {
+    e.preventDefault()
+    popup.classList.add('popup_hidden');
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value ;
+    
+  }); 
+
+
 // ----------Функции------------
 
 
-function openPopup() {
-    popup.classList.remove('popup_hidden');
-    document.addEventListener('keypress');
-}
+// function openPopup() {
+//     popup.classList.remove('popup_hidden');
+//     document.addEventListener('keypress');
+// }
 
-function closePopup() {
-    popup.classList.add('popup_hidden');
-    document.removeEventListener('keypress');
-}
+// function closePopup() {
+//     popup.classList.add('popup_hidden');
+//     document.removeEventListener('keypress');
+// }
 
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
+// function formSubmitHandler (evt) {
+//     evt.preventDefault();
 
        
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value ;
+//     profileName.textContent = nameInput.value;
+//     profileJob.textContent = jobInput.value ;
 
-}
+// }
 
 
 // ----------Обработчики------------
 
 
-popupOpenButton.addEventListener('click', function () {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+// popupOpenButton.addEventListener('click', function () {
+//     nameInput.value = profileName.textContent;
+//     jobInput.value = profileJob.textContent;
 
-    openPopup();
+//     openPopup();
 
   
-})
+// })
 
-popupCloseButton.addEventListener('click', function () {
-    closePopup();
+// popupCloseButton.addEventListener('click', function () {
+//     closePopup();
 
 
-});
+// });
 
-popup.addEventListener('click', function (e) {
-    if (e.target === e.currentTarget) {
-    closePopup();
-    }
-    });
+// popup.addEventListener('click', function (e) {
+//     if (e.target === e.currentTarget) {
+//     closePopup();
+//     }
+//     });
 
  
-    formElement.addEventListener('submit', formSubmitHandler); 
+//     formElement.addEventListener('submit', formSubmitHandler); 
